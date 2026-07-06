33fcb6f89c1818e7f95aeb023fe315010e00a3db
Jonathan Nahmias <jnahmias83@gmail.com>
2026-07-06 17:09:05 +0300

    meetings_report.php: corrige le debordement du bouton legende en anglais (cellule trop etroite pour le texte), elargit et centre le texte a l'interieur

--- meetings_report.php
+++ meetings_report.php
@@ -556,12 +556,12 @@ if(@$is_colors) {
 	$new_task_legend_align = (@$lang == 'HE') ? 'left' : 'right';
 	$new_task_legend_page_w = $pdf->getPageWidth() - $pdf->getMargins()['left'] - $pdf->getMargins()['right'];
 	$new_task_legend_badge = '<table cellpadding="4" cellspacing="0"><tr><td style="background-color:'.@$global_bgcolor_new_task->bgcolor.';border:1px solid black;text-align:center;"><strong style="font-size:14px;">'.$new_task_legend_label.'</strong></td></tr></table>';
-	$new_task_legend_spacer = '<td width="85%">&nbsp;</td>';
-	$new_task_legend_cell = '<td width="15%">'.$new_task_legend_badge.'</td>';
+	$new_task_legend_spacer = '<td width="70%">&nbsp;</td>';
+	$new_task_legend_cell = '<td width="30%" style="text-align:center;">'.$new_task_legend_badge.'</td>';
 	// l'ordre source spacer+cellule donne le bon cote visuel dans les deux langues (le bidi hebreu/anglais
 	// du texte a l'interieur inverse deja le rendu visuel, meme a ordre source identique)
 	$new_task_legend_cells = $new_task_legend_spacer.$new_task_legend_cell;
-	$new_task_legend_html = '<tr><td></td><td><table width="'.$new_task_legend_page_w.'mm" cellpadding="0" cellspacing="0" dir="ltr"><tr>'.$new_task_legend_cells.'</tr></table></td></tr>';
+	$new_task_legend_html = '<tr><td colspan="2"><table width="'.$new_task_legend_page_w.'mm" cellpadding="0" cellspacing="0" dir="ltr"><tr>'.$new_task_legend_cells.'</tr></table></td></tr>';
 }
 
 $html1_body = '<tr style="font-size:16px;"><td width="40px;">&nbsp;</td><td style="text-align:center;"><span dir="'.$dir_table.'"><strong><u>'.$title.'</u></strong></span></td></tr>'.$new_task_legend_html.'</table>';
