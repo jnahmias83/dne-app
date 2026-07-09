0d6ef30fde5c2aa1e3a44ad3a4fc7f99d306ab3a
Jonathan Nahmias <jnahmias83@gmail.com>
2026-07-09 13:56:31 +0300

    add_custom_report.php: la langue de la page et du radio en mode update suit desormais rdv_lang (reunion) ou custom_report->lang au lieu de rester bloquee sur project->lang

--- add_custom_report.php
+++ add_custom_report.php
@@ -174,7 +174,19 @@ asort($responsibles_array);
 asort($pass_ons_array);
 asort($progress_status_array);
 
-$page_lang = !empty($project->lang) ? $project->lang : @$_SESSION['lang'];
+if ($id > 0) {
+	if (!empty($id_rdv_report) && !empty(@$rdv->rdv_lang)) {
+		$page_lang = $rdv->rdv_lang;
+	} elseif (@$custom_report->lang == 'EN' || @$custom_report->lang == 1) {
+		$page_lang = 'EN';
+	} elseif (@$custom_report->lang == 'HE' || @$custom_report->lang == 0) {
+		$page_lang = 'HE';
+	} else {
+		$page_lang = !empty($project->lang) ? $project->lang : @$_SESSION['lang'];
+	}
+} else {
+	$page_lang = !empty($project->lang) ? $project->lang : @$_SESSION['lang'];
+}
 
 if($page_lang == 'EN'){
   $dir = 'dir-ltr';	
@@ -552,9 +564,9 @@ include 'menu_tasks.php';
 							<div class="col-12">					
 								<strong><?=@$language_label?> ?</strong>
 								&nbsp;
-								<input type="radio" id="lang_he" name="lang" value="HE" <?php if(($id == 0 && @$project->lang != 'EN') || ($id > 0 && !$is_specific_filter && @$custom_report->lang == 'HE') || ($id > 0 && !$is_specific_filter && @$_SESSION['filter_lang'] == '' && @$custom_report->lang == 0) || ($id > 0 && $is_specific_filter && (@$custom_report->lang == 'HE' || @$custom_report->lang == 0))) echo "checked";?> />&nbsp; <?=@$hebrew_label?>
+								<input type="radio" id="lang_he" name="lang" value="HE" <?php if($page_lang != 'EN') echo "checked";?> />&nbsp; <?=@$hebrew_label?>
 								&nbsp;
-								<input type="radio" id="lang_en" name="lang" value="EN" <?php if(($id == 0 && @$project->lang == 'EN') || ($id > 0 && !$is_specific_filter && @$custom_report->lang == 'EN') || ($id > 0 && !$is_specific_filter && @$_SESSION['filter_lang'] == '' && @$custom_report->lang == 1) || ($id > 0 && $is_specific_filter && (@$custom_report->lang == 'EN' || @$custom_report->lang == 1))) echo "checked";?> />&nbsp; <?=@$english_label?>
+								<input type="radio" id="lang_en" name="lang" value="EN" <?php if($page_lang == 'EN') echo "checked";?> />&nbsp; <?=@$english_label?>
 							</div>
 						</div>
 						
