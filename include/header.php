4aee84099bd799f5b9d7c9442730900624d6ffe9
Jonathan Nahmias <jnahmias83@gmail.com>
2026-07-12 16:58:45 +0300

    Ajoute la balise <meta name=viewport> manquante (include/header.php + index.php) pour activer le responsive existant (Bootstrap + @media deja presents) sur tablette/mobile

--- include/header.php
+++ include/header.php
@@ -33,6 +33,7 @@ $projects_list_array = explode(',',@$_SESSION['projects_list']);
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>DNE</title>
 
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
